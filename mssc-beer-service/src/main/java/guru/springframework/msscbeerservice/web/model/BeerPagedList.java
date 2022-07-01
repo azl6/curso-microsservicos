d6ebcd74b1ffc0895bd889496a0b41c2a068d10a
Alex <alex.rodrigues23997@gmail.com>
2022-06-30 22:31:28 -0300

    42: Data model

---
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import org.springframework.data.domain.*;
+
+import java.util.List;
+
+public class BeerPagedList extends PageImpl<BeerDto> {
+
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDto> content) {
+        super(content);
+    }
+}
